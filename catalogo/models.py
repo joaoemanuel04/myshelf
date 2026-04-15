0c69204a03d2387ba4bab003eda524395657bdf2
joaoemanuel04 <jemanuel334@gmail.com>
2026-04-15 15:09:43 -0300

    Configuração final do banco de dados

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,83 +1,68 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
-class Avaliao(models.Model):
-    idavaliaþÒo = models.IntegerField(db_column='idAvaliaþÒo', primary_key=True)  # Field name made lowercase.
-    nota = models.CharField(db_column='Nota', max_length=1, blank=True, null=True)  # Field name made lowercase.
-    filmes_idfilmes = models.ForeignKey('Filmes', models.DO_NOTHING, db_column='Filmes_idFilmes')  # Field name made lowercase.
-    usuario_idusuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='Usuario_idUsuario')  # Field name made lowercase.
-
-    class Meta:
-        managed =True
-        db_table = 'avaliaþÒo'
-
-
-class Filmes(models.Model):
-    idfilmes = models.IntegerField(db_column='idFilmes', primary_key=True)  # Field name made lowercase.
-    nome = models.CharField(db_column='Nome', max_length=180)  # Field name made lowercase.
-    ano = models.CharField(db_column='Ano', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    duraþÒo = models.TimeField(blank=True, null=True)
-    faixa_etßria = models.IntegerField(db_column='Faixa etßria', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    sinopse = models.TextField(db_column='Sinopse', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed =True
-        db_table = 'filmes'
-
-
-class FilmesFavoritos(models.Model):
-    idfilmefav = models.IntegerField(db_column='idFilmeFav', primary_key=True)  # Field name made lowercase.
-    filmes_idfilmes = models.ForeignKey(Filmes, models.DO_NOTHING, db_column='Filmes_idFilmes')  # Field name made lowercase.
-    usuario_idusuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='Usuario_idUsuario')  # Field name made lowercase.
-
-    class Meta:
-        managed =True
-        db_table = 'filmes_favoritos'
-
-
-class FilmesHasFilmes(models.Model):
-    pk = models.CompositePrimaryKey('Filmes_idFilmes', 'Filmes_idFilmes1')
-    filmes_idfilmes = models.ForeignKey(Filmes, models.DO_NOTHING, db_column='Filmes_idFilmes')  # Field name made lowercase.
-    filmes_idfilmes1 = models.ForeignKey(Filmes, models.DO_NOTHING, db_column='Filmes_idFilmes1', related_name='filmeshasfilmes_filmes_idfilmes1_set')  # Field name made lowercase.
-
-    class Meta:
-        managed =True
-        db_table = 'filmes_has_filmes'
-
-
-class FilmesHasGnero(models.Model):
-    id_filmes_genero = models.CharField(primary_key=True, max_length=45)
-    filmes_idfilmes = models.ForeignKey(Filmes, models.DO_NOTHING, db_column='Filmes_idFilmes')  # Field name made lowercase.
-    gÛnero_idgÛnero = models.ForeignKey('Genero', models.DO_NOTHING, db_column='GÛnero_idGÛnero')  # Field name made lowercase.
-
-    class Meta:
-        managed =True
-        db_table = 'filmes_has_gÛnero'
-
-
+# 1. Gênero 
 class Genero(models.Model):
-    idgÛnero = models.IntegerField(db_column='idGÛnero', primary_key=True)  # Field name made lowercase.
-    nome = models.CharField(db_column='Nome', max_length=45)  # Field name made lowercase.
+    id_genero = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=45, db_column='Nome')
+
+    def __str__(self):
+        return self.nome
 
     class Meta:
-        managed =True
         db_table = 'genero'
 
+# 2. Filmes
+class Filmes(models.Model):
+    id_filmes = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=180, db_column='Nome')
+    ano = models.CharField(max_length=4, blank=True, null=True, db_column='Ano')
+    duracao = models.TimeField(blank=True, null=True, db_column='duracao')
+    faixa_etaria = models.IntegerField(blank=True, null=True, db_column='faixa_etaria')
+    sinopse = models.TextField(blank=True, null=True, db_column='Sinopse')
+    
+    # Campo Extra para o seu Front-end 
+    url_capa = models.URLField(max_length=500, blank=True, null=True)
 
-class Usuario(models.Model):
-    idusuario = models.IntegerField(db_column='idUsuario', primary_key=True)  # Field name made lowercase.
-    nome = models.CharField(db_column='Nome', max_length=100)  # Field name made lowercase.
-    email = models.CharField(max_length=100)
-    senha = models.CharField(max_length=8)
-    nickname = models.CharField(db_column='Nickname', max_length=45)  # Field name made lowercase.
+    # Relacionamento Muitos-para-Muitos 
+    generos = models.ManyToManyField(Genero, related_name='filmes')
+
+    def __str__(self):
+        return self.nome
 
     class Meta:
-        managed =True
+        db_table = 'filmes'
+
+# 3. Usuário 
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, db_column='Nome')
+    email = models.EmailField(max_length=100, unique=True)
+    senha = models.CharField(max_length=128)
+    nickname = models.CharField(max_length=45, unique=True, db_column='Nickname')
+
+    def __str__(self):
+        return self.nickname
+
+    class Meta:
         db_table = 'usuario'
+
+# 4. Avaliação
+class Avaliacao(models.Model):
+    id_avaliacao = models.AutoField(primary_key=True)
+    NOTAS_CHOICES = [(i, str(i)) for i in range(1, 6)] 
+    nota = models.IntegerField(choices=NOTAS_CHOICES)
+    
+    filme = models.ForeignKey(Filmes, on_delete=models.CASCADE, related_name='avaliacoes')
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='avaliacoes')
+
+    class Meta:
+        db_table = 'avaliacao'
+
+# 5. Filmes Favoritos
+class FilmesFavoritos(models.Model):
+    id_filme_fav = models.AutoField(primary_key=True)
+    filme = models.ForeignKey(Filmes, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'filmes_favoritos'
