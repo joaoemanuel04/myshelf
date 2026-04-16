d9980efe194ca318e4ea728c3d30027dd2284467
Sannntos03 <theylllon22@gmail.com>
2026-04-15 21:40:55 -0300

    Criação do superusuário

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Filmes, Genero, Usuario, Avaliacao, FilmesFavoritos
 
-# Register your models here.
+admin.site.register(Filmes)
+admin.site.register(Genero)
+admin.site.register(Usuario)
+admin.site.register(Avaliacao)
+admin.site.register(FilmesFavoritos)
+
+
+ 
