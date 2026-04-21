09d0159f754df8e151ef0cada1c665506d2b0e1b
Sannntos03 <theylllon22@gmail.com>
2026-04-21 17:23:15 -0300

    Fix: Consertando rotas urls e classe de renderização home.

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class catalogoView(TemplateView):
-    template_name = 'homecatalogo.html'
+class CatalogoView(TemplateView):
+    template_name = 'catologo/homecatalogo.html'
 
 
